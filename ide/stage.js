6bcfbd1c113e3d179753416b14f8e2ea74762eb6
ruinpsilva <rui.nelson.p.silva@gmail.com>
2014-12-10 23:00:28 +0000

    Adicionado botão para passar do stage para o IDE de Diagrama de Classes

--- ide/stage.js
+++ ide/stage.js
@@ -1,5 +1,10 @@
 $(document).ready(function(){
-   $("#btnGoToIDECasosDeUso").click(function(){
+    $("#btnGoToIDECasosDeUso").click(function(){
         window.location.href = "casosUso.html";
     });
+
+    $("#btnGoToIDEDiagramaClasses").click(function(){
+        window.location.href = "diagramaClasses.html";
+    });
+
 });
