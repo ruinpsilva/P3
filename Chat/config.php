1f7fd7ce491a43191968d86970578140cda73626
ruinpsilva <rui.nelson.p.silva@gmail.com>
2015-02-08 19:24:55 +0000

    Limpeza

--- Chat/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-	// configuracoes de acesso a BD
-	define('DB_HOST', 'localhost');
-    define('DB_USER', 'superP3');
-    define('DB_PASSWORD', 'teste');
-    define('DB_DATABASE', 'p3chat2');
-    // 15 segundos de timeout
-    define('SESSION_TIMEOUT',28800);
-?>
--- Chat/no-cache-headers.php
+++
@@ -1,7 +0,0 @@
-<?php
-header("Expires: Mon, 26 Jul 1997 05:00:00 GMT");		    // data no passado
-header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT");	// data actual
-header("Cache-Control: no-store, no-cache, must-revalidate");	// controlo de cache
-header("Cache-Control: post-check=0, pre-check=0", false);		// controlo de cache
-header("Pragma: no-cache");						// controlo de cache
-?>
